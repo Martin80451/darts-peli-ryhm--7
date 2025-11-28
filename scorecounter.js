b94f1d0b57f086270d32a80969dfa15cb5456289
Martin80451 <58072929+Martin80451@users.noreply.github.com>
2025-11-28 12:40:40 +0200

    Turn based point adding
    - Added logic for turn based point adding

--- scorecounter.js
+++ scorecounter.js
@@ -8,10 +8,26 @@ let turns = 0;
 
 function addToScore(e) {
     console.log(scoreInput.value);
+    let player = decidePlayer();
+    player.unshift(scoreInput.value);
     turns++;
+    console.log(players);
     e.preventDefault();
 }
 
+// Not scalable yet
+function decidePlayer() {
+    switch (turns % playerCount) {
+        case 0:
+            return players[0];
+        case 1:
+            return players[1];
+        case 2:
+            return players[2];
+        case 3:
+            return players[3];
+    }
+}
 
 const form = document.getElementById("newScoreInput");
 form?.addEventListener("submit", addToScore);
