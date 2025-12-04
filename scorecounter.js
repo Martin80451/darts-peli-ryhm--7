83d5063bcbad6fa10bf346651e83705f8f7a614a
Martin80451 <martin.malmivirtahky@gmail.com>
2025-12-04 22:19:24 +0200

    Player points are now being added to the table

--- scorecounter.js
+++ scorecounter.js
@@ -6,20 +6,31 @@ for (let i = 0; i < playerCount; i++) {
 const scoreInput = document.getElementById("score");
 const player1Header = document.getElementById("player1Name");
 const player2Header = document.getElementById("player2Name");
+const player1Table = document.getElementById("player1Score");
+const player2Table = document.getElementById("player2Score");
 const playerHeaders = [player1Header, player2Header];
+const playerTables = [player1Table, player2Table];
 let turns = 0;
 
 function addToScore(e) {
   console.log(scoreInput.value);
   let player = decidePlayer(players);
   let header = decidePlayer(playerHeaders);
+  let table = decidePlayer(playerTables);
   player.unshift(scoreInput.value);
+  let newestTenPoints = player.slice(0, 11);
   header.classList.add("activeBorder");
   header.classList.remove("noBorder");
   let otherHeaders = playerHeaders.filter((h) => h !== header);
   for (let i = 0; i < playerHeaders.length; i++) {
     otherHeaders[0].classList.remove("activeBorder");
     otherHeaders[0].classList.add("noBorder");
+  }
+
+  for (let j = 0; j < table.rows.length; j++) {
+    table.rows[j].cells[0].innerHTML = newestTenPoints[j]
+      ? newestTenPoints[j]
+      : "";
   }
   turns++;
   console.log(players);
